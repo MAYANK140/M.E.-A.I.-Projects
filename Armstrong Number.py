b1ee6616e0845f96f1da40734b8f24703a25fb3c
MAYANK140 <21MTM3003@cuchd.in>
2022-09-12 14:59:27 +0100

    Here, I have completed the code for verifying an Armstrong Number

--- Armstrong Number.py
+++ Armstrong Number.py
@@ -1,1 +1,16 @@
+n = int(input("Enter a number"))
+sum = 0
+cpN = n
+Order = len(str(n))
+while (n > 0):
+    a = n % 10
+    sum += a ** Order
+    n = n // 10
+if sum == cpN:
+    print("Yes, it is an Armstrong Number")
+else:
+    print("No, it is not an Armstrong Number")
+
+
+
 
