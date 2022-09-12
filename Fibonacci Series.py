7582f2c976663544b16941cb7a85b78701e9468e
MAYANK140 <21MTM3003@cuchd.in>
2022-09-12 13:05:59 +0100

    Herer I have written the code of Fibonacci Series stored in an array of given length.

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,7 +1,10 @@
 n=int(input("Enter the number of digits you want to see in the Fibonacci series"))
 a,b,c=0,1,1
+m=[]
 for i in range(n):
     print(c, "\n")
     c=a+b
     a=b
     b=c
+    m.append(c)
+print(m)
