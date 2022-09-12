2ebc633ad60804da526b4ea31c3a0704d0d0e6d8
MAYANK140 <21MTM3003@cuchd.in>
2022-09-12 13:46:38 +0100

    I have updated it into a Palindrome Code.

--- Reverse String.py
+++ Reverse String.py
@@ -1,1 +1,8 @@
-
+ex_str = "12321"
+rev = ''
+for i in range(len(ex_str)-1,-1,-1):
+    rev = rev + ex_str[i]
+if rev == ex_str:
+    print("It is a Palindrome")
+else:
+    print("No, its not a Palindrome")
